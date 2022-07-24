8bb5aa62a5295a0ebc4494f66d677a5a2af15161
ChaosKingNV <namannaman.v32@gmail.com>
2022-07-24 16:23:44 +0530

    modified:   Console/main.cpp 	modified:   build-Console-Desktop_Qt_5_15_2_MinGW_32_bit-Debug/debug/Console.exe 	modified:   build-Console-Desktop_Qt_5_15_2_MinGW_32_bit-Debug/debug/main.o

--- Console/main.cpp
+++ Console/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void play_game()
+{
+    int random = (rand() % 10) + 1;
+    cout << " Guess a number between 1 to 10: ";
+    int guess;
+
+    while (true) {
+        cin >> guess;
+
+        if (guess == random) {
+           cout << "You win\n";
+           break;
+        }
+        else if (guess < random) {
+            cout << "You are too low\n";
+        }
+        else {
+            cout << "Your are too high\n";
+        }
+    }
+
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    srand(time(NULL));
+
+    int choice;
+    cout << "1. Play Game " << "2. Quit \n";
+
+    cin >> choice;
+    switch(choice)
+    {
+        case 1:
+            play_game();
+            break;
+        case 2:
+            cout << "quiting........ \n";
+            break;
+    }
 }
